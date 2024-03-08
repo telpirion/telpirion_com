e0f541622e2fd9acf44e48385912a8d4e68bd305
Eric Schmidt <em.schmidt78@gmail.com>
2024-03-07 22:39:10 -0800

    feat: updated Code samples for cards

--- src/app/code-samples/code-samples.component.ts
+++ src/app/code-samples/code-samples.component.ts
@@ -17,8 +17,44 @@ export class CodeSamplesComponent implements OnInit {
   content: string = 'I have produced the following code samples and ' +
                     'demonstrations.';
 
-  // TODO: Turn this into a GitHub API call?
+  // TODO(telpirion): Turn this into a GitHub API call?
   samples: Sample[] = [
+    {
+      title: 'Cloud Datastore client features',
+      url: 'https://github.com/googleapis/google-cloud-go/pulls?q=is%3Apr+is%3Amerged+author%3Atelpirion+label%3A%22api%3A+datastore%22',
+      lang: ['Go'],
+      technologies: ['Google Cloud', 'Google Cloud Bigtable']
+    },
+    {
+      title: 'Cloud Firestore client features',
+      url: 'https://github.com/googleapis/google-cloud-go/pulls?q=is%3Apr+is%3Amerged+author%3Atelpirion+label%3A%22api%3A+firestore%22',
+      lang: ['Go'],
+      technologies: ['Google Cloud', 'Google Cloud Bigtable']
+    },
+    {
+      title: 'Cloud Bigtable client features',
+      url: 'https://github.com/googleapis/google-cloud-go/pulls?q=is%3Apr+is%3Amerged+author%3Atelpirion+label%3A%22api%3A+bigtable%22',
+      lang: ['Go'],
+      technologies: ['Google Cloud', 'Google Cloud Bigtable']
+    },
+    {
+      title: 'Cloud Firestore Bulkwriter',
+      url: 'https://github.com/googleapis/google-cloud-go/pull/5946',
+      lang: ['Go'],
+      technologies: ['Google Cloud', 'Google Cloud Firestore']
+    },
+    {
+      title: 'Cloud Bigtable CLI migration, formatting feature, protocol buffers',
+      url: 'https://github.com/googleapis/cloud-bigtable-cbt-cli/pulls?q=is%3Apr+is%3Amerged+author%3Atelpirion+',
+      lang: ['Go'],
+      technologies: ['Google Cloud', 'Google Cloud Bigtable', 'protocol buffers']
+    },
+    {
+      title: 'Python SDK for Vertex AI, client library and samples',
+      url: 'https://github.com/googleapis/python-aiplatform/pulls?q=is%3Apr+is%3Amerged+author%3Atelpirion',
+      lang: ['Python'],
+      technologies: ['Google Cloud', 'Google Cloud Vertex AI', 'protocol buffers']
+    },
     {
       title: 'Product Search samples',
       url: 'https://github.com/GoogleCloudPlatform/dotnet-docs-samples/pull/718',
@@ -74,6 +110,11 @@ export class CodeSamplesComponent implements OnInit {
       technologies: ['Google Hangouts Chat', 'Google AppsScript']
     },
   ];
+
+  view(sample : Sample) {
+    console.log(`View code: ${sample.title}`);
+    window.location.href = sample.url;
+  }
 
   constructor() { }
 
