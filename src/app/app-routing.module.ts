01f7e32b124d8a5a11b7c117cc3974c6a85e7b97
Eric Schmidt <em.schmidt78@gmail.com>
2024-03-14 20:35:58 -0700

    feat: added 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,19 @@ import { GamesComponent } from './games/games.ng';
 import { HomeComponent } from './home/home.ng';
 import { PublicationsComponent } from './publications/publications.component';
 import { CodeSamplesComponent } from './code-samples/code-samples.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.ng';
+import { PlayComponent } from './play/play.ng';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'about', component: AboutComponent },
-  { path: 'games', component: GamesComponent },
   { path: 'apps', component: AppsPageComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'publications', component: PublicationsComponent },
   { path: 'code-samples', component: CodeSamplesComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'games', component: GamesComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'play', component: PlayComponent},
+  { path: 'publications', component: PublicationsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
---
+++ src/app/page-not-found/page-not-found.ng.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-not-found',
+    templateUrl: './page-not-found.ng.html',
+    styleUrls: ['./page-not-found.ng.css']
+})
+export class PageNotFoundComponent {
+    title: string = "Page not found";
+}
